4f5b8bc0d798363829c51c9fa0731f44524b8617
Harrison Wright <harrison_wright@partech.com>
2023-08-25 16:36:36 -0500

    added some comments for fetchComponents endpoint

--- ReactApp/src/services/openAIApi.tsx
+++ ReactApp/src/services/openAIApi.tsx
@@ -18,6 +18,21 @@ export default class Api {
 
     public async fetchComponents(userInput: string = "") {
         try {
+            /*
+            todo: establish a prompt chain (? need to look into how it works. basically just a 
+            series of promps that kinda establishes a convo. we'll need to plug the user input in there
+            somewhere, possibly in multiple places)
+
+            first thoguht
+            1. here's all the components.(json of components) I'm building an interface for a back end of a restaurant web blalblabla I'm going to ask you sometihng in a minute
+            2. Given these, I'm a {user input} and I mostly care about {user input, maybe a pre-canned description of the job} 
+                Which of these components would be most useful on my dashboard? Please respond with JSON that includes only elements from the list I provided you previously
+                Please avoid very similar components (i.e. sales with labor costs and sales without labor costs)
+            3. Could you arrange that list in order of importance? (or some other post processing of the list. Maybe have it double check that each component exists in the map from the first question?)
+
+            we can add caveats and change wording and what not 
+            
+            */
             const components = await fetch("/components", { method: "POST" });
             console.log(components);
             return components.json();
