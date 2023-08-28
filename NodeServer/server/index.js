02c8e7e2eaaa0a6b34b7205be23ab936410a9a93
Harrison Wright <harrison_wright@partech.com>
2023-08-28 11:48:15 -0500

    basic chain in

--- NodeServer/server/index.js
+++ NodeServer/server/index.js
@@ -8,12 +8,13 @@ const _dashboards = require('./dashboards');
 
 const { OpenAI } = require('langchain/llms/openai');
 const { ChatOpenAI } = require('langchain/chat_models/openai');
-const { LLMChain, SimpleSequentialChain } = require('langchain/chains');
+const { LLMChain, SequentialChain } = require('langchain/chains');
 const { PromptTemplate } = require('langchain/prompts');
-const { BufferMemory } = require('langchain/memory');
+const { BufferMemory, SimpleMemory } = require('langchain/memory');
 
 const llm = new OpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0.5
 })
 
 const chat = new ChatOpenAI({
@@ -44,82 +45,52 @@ Please respond with JSON that includes only elements from the list I provided yo
 Please avoid very similar components (i.e. sales with labor costs and sales without labor costs).`
 
 const setupPrompt = `Hey There! We're working on setting up some dashboards for restuarant back-of-house users.
-Given this list of components {_components}, here are some sample dashboards for different users {_dashboards}. I'm going to ask you something soon`
+Given this list of components {componentsExample}, here are some sample dashboards for different users {dashboardsExample}.
+ I'm going to ask you something soon, could you provide some key takeaways for yourself that might help later?`
 
 const actionPrompt = `
 Given these components and sample dashboards, I'm a {role} and my areas of concern are mostly {problemAreas}. Could you suggest a dashboard that would fit my needs best?
-Please respond with JSON using only the components I've provided in the original list. Try to avoid very similar or duplicate components (i.e. sales with labor costs and sales without labor costs).
+Please respond with JSON using only the components I've provided here - {componentsExample}. Try to avoid very similar or duplicate components (i.e. sales with labor costs and sales without labor costs).
 `
-
 app.post('/components', async (req, res) => {
     const memory = new BufferMemory();
-    // const prompt = PromptTemplate.fromTemplate(prompt1);
 
-    var setupPromptTemplate = new PromptTemplate({
-        prompt: setupPrompt,
-        inputVariables: ["components", "dashboards"]
-    });
+    const setupPromptTemplate = PromptTemplate.fromTemplate(setupPrompt);
+    const actionPromptTemplate = PromptTemplate.fromTemplate(actionPrompt);
 
     const setupChain = new LLMChain({
         llm,
         prompt: setupPromptTemplate,
-    });
-
-    const actionPromptTemplate = new PromptTemplate({
-        prompt: actionPrompt,
-        inputVariables: ["role", "problemAreas"]
+        inputVariables: ["componentsExample", "dashboardsExample"],
+        outputKey: "setupResult"
     });
 
     const actionChain = new LLMChain({
         llm,
         prompt: actionPromptTemplate,
+        inputVariables: ["role", "problemAreas", "setupResult", "componentsExample"],
+        outputKey: "components"
     });
 
-    const overallChain = new SimpleSequentialChain({
+    const overallChain = new SequentialChain({
         chains: [setupChain, actionChain],
+        inputVariables: ["componentsExample", "dashboardsExample", "role", "problemAreas"],
         verbose: true,
-        memory
+        outputVariables: ["components"]
     });
 
-    var res = overallChain.call({
-        _components,
-        _dashboards,
+    const inputValues = {
+        componentsExample: JSON.stringify(_components),
+        dashboardsExample: JSON.stringify(_dashboards),
         role: req.body.role,
-        problemAreas: req.body.problemAreas
-    });
-    // const chain = new LLMChain({
-    //     llm,
-    //     prompt,
-    //     memory
-    // });
+        problemAreas: req.body.problemAreas,
+    };
 
-    // const result = await chain.call({
-    //     role: req.body.role,
-    //     componentsJson: JSON.stringify(_components),
-    //     problemAreas: req.body.problemAreas
-    // });
+    const result = await overallChain.call(inputValues);
 
-    // var formatted = JSON.parse(result['text']);
-
-    /*
-    todo: establish a prompt chain (? need to look into how it works. basically just a 
-    series of promps that kinda establishes a convo. we'll need to plug the user input in there
-    somewhere, possibly in multiple places)
-
-    first thoguht
-    1. here's all the components.(json of components) I'm building an interface for a back end of a restaurant web blalblabla I'm going to ask you sometihng in a minute
-    2. Given these, I'm a {user input} and I mostly care about {user input, maybe a pre-canned description of the job} 
-        Which of these components would be most useful on my dashboard? Please respond with JSON that includes only elements from the list I provided you previously
-        Please avoid very similar components (i.e. sales with labor costs and sales without labor costs)
-    3. Could you arrange that list in order of importance? (or some other post processing of the list. Maybe have it double check that each component exists in the map from the first question?)
-
-    we can add caveats and change wording and what not 
-    
-    */
-    res.json({ components: formatted })
+    res.send({ components: JSON.parse(result['components']) });
 
 });
-
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
