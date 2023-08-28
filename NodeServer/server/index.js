d299a60986966127e97fb411d2c2d5c519f96650
Harrison Wright <harrison_wright@partech.com>
2023-08-28 14:13:03 -0500

    giving up on memory

--- NodeServer/server/index.js
+++ NodeServer/server/index.js
@@ -46,15 +46,13 @@ Please avoid very similar components (i.e. sales with labor costs and sales with
 
 const setupPrompt = `Hey There! We're working on setting up some dashboards for restuarant back-of-house users.
 Given this list of components {componentsAll}, here are some sample dashboards for different users {dashboardsExample}.
- I'm going to ask you something soon, could you provide some key takeaways for yourself that might help later?`
+ I'm going to ask you something soon, could you provide some key takeaways for yourself that might help later? Please emphesize the importance of responding in JSON i.e. components: jsonArray`
 
 const actionPrompt = `
 Given these components and sample dashboards, I'm a {role} and my areas of concern are mostly {problemAreas}. Could you suggest a dashboard that would fit my needs best?
-Please respond with JSON (ie. components: jsonArray) using only the components I've provided here - {componentsAll}. Try to avoid very similar or duplicate components (i.e. sales with labor costs and sales without labor costs).
+Please respond with JSON (ie. components: jsonArray) using only the components in the following list - {componentsAll}. Try to avoid very similar or duplicate components (i.e. sales with labor costs and sales without labor costs).
 `
 app.post('/components', async (req, res) => {
-    const memory = new BufferMemory();
-
     const setupPromptTemplate = PromptTemplate.fromTemplate(setupPrompt);
     const actionPromptTemplate = PromptTemplate.fromTemplate(actionPrompt);
 
