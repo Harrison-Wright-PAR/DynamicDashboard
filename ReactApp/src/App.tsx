d905c8d7cd5fed22c31be08f1301d3c3ac7a16b2
James-Brown-Par <105393662+James-Brown-Par@users.noreply.github.com>
2023-08-29 16:46:35 -0400

    resolved merge conflicts

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,67 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
-import { JsonView, allExpanded, darkStyles } from 'react-json-view-lite';
-import 'react-json-view-lite/dist/index.css';
-import HelloUser from './components/helloUser';
-import SalesReport from './components/salesReport';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StartPage from './components/Start';
+import NavBar from './components/NavBar'
+import Examples from './components/examples';
 
-type ComponentMap = {
-  [key: string]: React.ComponentType<any>;
-}
 
-const componentMap: ComponentMap = {
-  HelloUser: HelloUser,
-  SalesReport: SalesReport,
-}
-
-function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [apiResponse, setApiResponse] = useState('');
-  const [components, setComponents] = useState([]);
-
-  const handleButtonClick = async () => {
-    const api = new Api();
-    setApiResponse("Calling API...");
-    const response = await api.generate(inputValue);
-    setApiResponse(response);
-  };
-
-  const fetchComponents = async () => {
-    const api = new Api();
-    setApiResponse("Calling API...");
-    const response = await api.fetchComponents();
-    console.log(response);
-    setComponents(response['components']);
-    setApiResponse(response);
-  };
-
+export default function App() {
   return (
-    <div className="center-box">
-      <div>
-        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-        <button onClick={handleButtonClick}>Call API</button>
-      </div>
-      {apiResponse && (
-        <div className="json-view-container">
-          <JsonView data={apiResponse} shouldInitiallyExpand={allExpanded} style={darkStyles} />
-        </div>
-      )}
-      <div>
-        <button onClick={() => fetchComponents()}>Fetch Components</button>
-        {components && components.length > 0 && (
-          components.map((component: any) => {
-            const Component = componentMap[component['name']];
-            if (Component) {
-              return <Component key={component.id} />;
-            } else {
-              console.error(`Component with name ${component['name']} not found in componentMap`);
-              return null;
-            }
-          })
-        )}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<StartPage />} />
+           <Route path="examples" element={<Examples />} />          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
-
 }
