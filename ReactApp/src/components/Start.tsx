53c23d5e840f4c226e53a417bb6b0875e5919075
Kyle-SenkevichPAR <kyle.senkevich@partech.com>
2023-08-30 13:09:15 -0400

    Added developer tools

--- ReactApp/src/components/Start.tsx
+++ ReactApp/src/components/Start.tsx
@@ -4,7 +4,6 @@ import Api from "../services/openAIApi";
 import { JsonView, allExpanded, darkStyles } from "react-json-view-lite";
 import "react-json-view-lite/dist/index.css";
 import HelloUser from "./helloUser";
-import SalesReport from "./salesReport";
 import SalesTrendReport from "./salesTrendReport";
 import {
   Button,
@@ -14,7 +13,10 @@ import {
   Grid,
   getListItemSecondaryActionClassesUtilityClass,
   Typography,
-  LinearProgress
+  LinearProgress,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
 import { WidthProvider, Responsive } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
@@ -28,6 +30,7 @@ import MenuChangesReport from "./menuChangesReport";
 import SalesDailyReport from "./salesDailyReport";
 import LaborCostReport from "./laborCostReport";
 import { layout, LayoutItem } from "../gridLayouts";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type ComponentMap = {
   [key: string]: React.ComponentType<any>;
@@ -35,7 +38,6 @@ type ComponentMap = {
 
 const componentMap: ComponentMap = {
   HelloUser: HelloUser,
-  SalesReport: SalesReport,
   SalesTrendReport: SalesTrendReport,
   LaborCostReport: LaborCostReport,
   LaborIrregularitiesReport: LaborIrregularitiesReport,
@@ -51,7 +53,9 @@ function StartPage() {
   const [dashboard, setDashboard] = useState("");
   const [userRequest, setUserRequest] = useState("");
   const [loading, setLoading] = useState(false);
-  const [loadingMessage, setLoadingMessage] = useState("If you wait for it, it will come... Crafting your Board of Dreams now! 🌟📊");
+  const [loadingMessage, setLoadingMessage] = useState(
+    "If you wait for it, it will come... Crafting your Board of Dreams now! 🌟📊"
+  );
   const [dashboardGenerated, setDashboardGenerated] = useState(false);
   const api = new Api();
 
@@ -59,13 +63,12 @@ function StartPage() {
 
   const layout: Array<LayoutItem> = [
     { i: "HelloUser", x: 0, y: 0, w: 6, h: 4 },
-    { i: "SalesReport", x: 1, y: 1, w: 6, h: 4 },
-    { i: "SalesTrendReport", x: 2, y: 2, w: 7.7, h: 10.2 },
-    { i: "LaborCostReport", x: 3, y: 3, w: 11, h: 15 },
-    { i: "LaborIrregularitiesReport", x: 4, y: 4, w: 9.1, h: 11.5 },
-    { i: "LiveSalesReport", x: 5, y: 5, w: 4, h: 4 },
-    { i: "MenuChangesReport", x: 6, y: 6, w: 8.5, h: 11.5 },
-    { i: "SalesDailyReport", x: 7, y: 7, w: 11, h: 15 },
+    { i: "SalesTrendReport", x: 1, y: 1, w: 7.7, h: 10.2 },
+    { i: "LaborCostReport", x: 2, y: 2, w: 11, h: 15 },
+    { i: "LaborIrregularitiesReport", x: 3, y: 3, w: 9.1, h: 11.5 },
+    { i: "LiveSalesReport", x: 4, y: 4, w: 4, h: 4 },
+    { i: "MenuChangesReport", x: 5, y: 5, w: 8.5, h: 11.5 },
+    { i: "SalesDailyReport", x: 6, y: 6, w: 11, h: 15 },
   ];
 
   const handleButtonClick = async () => {
@@ -116,6 +119,8 @@ function StartPage() {
   };
 
   const updateDashboard = async () => {
+    setLoading(true);
+    setLoadingMessage("If you wait for it, it will come... Crafting your Board of Dreams now! 🌟📊");
     var res = await api.updateDashboard(dashboard, userRequest);
     console.log(res);
     var components = res["components"];
@@ -128,6 +133,7 @@ function StartPage() {
     }
 
     setApiResponse(res);
+    setLoading(false);
   };
 
   const fetchComponentsLocal = async () => {
@@ -190,14 +196,16 @@ function StartPage() {
   useEffect(() => {
     const timeoutId1 = setTimeout(() => {
       setLoadingMessage("Good boards come to those who wait...🙇‍♂️📈");
-  
+
       const timeoutId2 = setTimeout(() => {
-        setLoadingMessage("“I don’t always load, but when I do - it takes just a minute. 😎🏖️” - Most interesting board in the world");
+        setLoadingMessage(
+          "“I don’t always load, but when I do - it takes just a minute. 😎🏖️” - Most interesting board in the world"
+        );
       }, 20000); // change the message after another 20 seconds (20000 milliseconds)
-  
+
       return () => clearTimeout(timeoutId2);
     }, 20000); // change the message after 20 seconds (20000 milliseconds)
-  
+
     return () => clearTimeout(timeoutId1);
   }, []); // run the effect only once, when the component mounts
 
@@ -216,51 +224,51 @@ function StartPage() {
         >
           {!dashboardGenerated && (
             <div>
-          <Box sx={{ mt: 3, alignItems: "left" }}>
-            <Typography
-              variant="h4"
-              sx={{
-                color: "parBlue",
-                fontWeight: "bold",
-                marginBottom: "10px",
-              }}
-            >
-              Welcome to the Board of Dreams!
-            </Typography>
-            <Typography variant="body1">
-              Tell us what you need to see to manage your brand in plain
-              english!
-            </Typography>
-            <Typography variant="body2" sx={{ color: "text.secondary" }}>
-              (Example: I need to be able to quickly see sales and labor, top
-              items sold, and device health.)
-            </Typography>
-          </Box>
-          <Box sx={{ mt: 3, width: "100%" }}>
-            {" "}
-            {/* add the Box component and set the flexDirection property */}
-            <TextField
-              sx={{ width: "100%", marginBottom: "10px" }}
-              rows={3}
-              value={inputValue}
-              multiline={true}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-              <Button
-                color="parBlue"
-                variant="contained"
-                disabled={loading}
-                onClick={fetchComponents}
-              >
-                Next
-              </Button>
-            </Box>
-          </Box>
-          </div>
+              <Box sx={{ mt: 3, alignItems: "left" }}>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    color: "parBlue",
+                    fontWeight: "bold",
+                    marginBottom: "10px",
+                  }}
+                >
+                  Welcome to the Board of Dreams!
+                </Typography>
+                <Typography variant="body1">
+                  Tell us what you need to see to manage your brand in plain
+                  english!
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  (Example: I need to be able to quickly see sales and labor,
+                  top items sold, and device health.)
+                </Typography>
+              </Box>
+              <Box sx={{ mt: 3, width: "100%" }}>
+                {" "}
+                {/* add the Box component and set the flexDirection property */}
+                <TextField
+                  sx={{ width: "100%", marginBottom: "10px" }}
+                  rows={3}
+                  value={inputValue}
+                  multiline={true}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                  <Button
+                    color="parBlue"
+                    variant="contained"
+                    disabled={loading}
+                    onClick={fetchComponents}
+                  >
+                    Next
+                  </Button>
+                </Box>
+              </Box>
+            </div>
           )}
           {loading && (
-            <Box sx={{ mt: 10, width: '100%' }}>
+            <Box sx={{ mt: 10, width: "100%" }}>
               <Typography variant="body1">{loadingMessage}</Typography>
               <LinearProgress />
             </Box>
@@ -271,73 +279,84 @@ function StartPage() {
           </Box> */}
           {dashboardGenerated && (
             <div>
-            <Box sx={{ mt: 3, width: '100%' }}>
-              <ResponsiveGridLayout
-                className="layout"
-                layouts={{ lg: layout }}
-                breakpoints={{ lg: 1600 }}
-                cols={{ lg: 16 }}
-                rowHeight={30}
-              >
-                {components &&
-                  components.length > 0 &&
-                  components.map((component: any) => {
-                    const Component = componentMap[component["name"]];
-                    if (Component) {
-                      return (
-                        <div
-                          key={component.id}
-                          data-grid={{ ...component.layout }}
-                        >
-                          <Component />
-                        </div>
-                      );
-                    } else {
-                      console.error(
-                        `Component with name ${component["name"]} not found in componentMap`
-                      );
-                      return null;
-                    }
-                  })}
-              </ResponsiveGridLayout>
-            </Box>
-            <Box sx={{ mt: 3, width: "100%" }}>
-              <Typography variant="body1">
-                Not happy with the dashboard? Request updates and regenerate the
-                dashboard.
-              </Typography>
-              {/* <Box sx={{ mt: 3 }}>
+              <Box sx={{ mt: 3, width: "100%" }}>
+                <ResponsiveGridLayout
+                  className="layout"
+                  layouts={{ lg: layout }}
+                  breakpoints={{ lg: 1600 }}
+                  cols={{ lg: 16 }}
+                  rowHeight={30}
+                >
+                  {components &&
+                    components.length > 0 &&
+                    components.map((component: any) => {
+                      const Component = componentMap[component["name"]];
+                      if (Component) {
+                        return (
+                          <div
+                            key={component.id}
+                            data-grid={{ ...component.layout }}
+                          >
+                            <Component />
+                          </div>
+                        );
+                      } else {
+                        console.error(
+                          `Component with name ${component["name"]} not found in componentMap`
+                        );
+                        return null;
+                      }
+                    })}
+                </ResponsiveGridLayout>
+              </Box>
+              <Box sx={{ mt: 3, width: "100%" }}>
+                <Typography variant="body1">
+                  Not happy with the dashboard? Request updates and regenerate
+                  the dashboard.
+                </Typography>
+                {/* <Box sx={{ mt: 3 }}>
               <Button color='parBlue' variant="contained" sx={{ margin: '10px' }} onClick={() => updateDashboard()}>Update Dashboard</Button>
               <TextField label="Any requests for updates?" value={userRequest} onChange={(e) => setUserRequest(e.target.value)} />
             </Box> */}
-              <Box sx={{ mt: 1, width: "100%" }}>
-                {" "}
-                {/* add the Box component and set the flexDirection property */}
-                <TextField
-                  sx={{ width: "100%", marginBottom: "10px" }}
-                  rows={3}
-                  value={userRequest}
-                  onChange={(e) => setUserRequest(e.target.value)}
-                  multiline={true}
-                />
-                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Button
-                    color="parBlue"
-                    variant="contained"
-                    onClick={updateDashboard}
-                  >
-                    Update Dashboard
-                  </Button>
+                <Box sx={{ mt: 1, width: "100%" }}>
+                  {" "}
+                  {/* add the Box component and set the flexDirection property */}
+                  <TextField
+                    sx={{ width: "100%", marginBottom: "10px" }}
+                    rows={3}
+                    value={userRequest}
+                    onChange={(e) => setUserRequest(e.target.value)}
+                    multiline={true}
+                  />
+                  <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Button
+                      color="parBlue"
+                      variant="contained"
+                      onClick={updateDashboard}
+                      disabled={loading}
+                    >
+                      Update Dashboard
+                    </Button>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-            <Box sx={{ mt: 3 }}>
-              <JsonView
-                data={apiResponse}
-                shouldInitiallyExpand={allExpanded}
-                style={darkStyles}
-              />
-            </Box>
+              <Box sx={{ mt: 20 }}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>Developer Tools</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <JsonView
+                      data={apiResponse}
+                      shouldInitiallyExpand={allExpanded}
+                    />
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
             </div>
           )}
         </Box>
